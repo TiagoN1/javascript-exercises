41ced5ed2f55b74faaa78f9f62bde19b1150379b
TiagoN1 <tiagoapneto1@gmail.com>
2024-09-03 13:00:25 +0100

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, numberOfTimes) {
+    let repeateadstring = ""
+    if (numberOfTimes < 0) return "ERROR";
+    for (let index = 0; index < numberOfTimes; index++) {
+        repeateadstring = repeateadstring + str
+    }
+    return repeateadstring
 };
 
 // Do not edit below this line
