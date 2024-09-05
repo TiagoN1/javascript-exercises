b824e87e15921cda843c59c22d7bdaf3f6139f63
TiagoN1 <tiagoapneto1@gmail.com>
2024-09-05 21:08:51 +0100

    Exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if ((Number.isInteger(num1) && num1 > 0) && (Number.isInteger(num2) && num2 > 0)) {
+        //Determine which number is higher
+        let max = num1 > num2 ? num1 : num2;
+        let min = num1 < num2 ? num1 : num2;
+        let sum = 0;
+        for (let index = min; index <= max; index++) {
+            sum = sum + index
 
+        }
+        return sum
+    }
+    else
+        return "ERROR"
 };
 
 // Do not edit below this line
