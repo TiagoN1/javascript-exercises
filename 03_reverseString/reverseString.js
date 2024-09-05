05984a86ee0197e39e4ececa6e13d18383cf33d9
TiagoN1 <tiagoapneto1@gmail.com>
2024-09-05 19:51:55 +0100

    Exercise 3 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (strToReverse) {
+    let b = ''
+    for (let index = 0; index < strToReverse.length; index++) {
+        b = b + strToReverse[strToReverse.length - index - 1]
 
+    }
+    return b
 };
 
 // Do not edit below this line
