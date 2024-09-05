ca4e3e6ee651ae20c8fd74196a7d2b89ee8e7f5d
TiagoN1 <tiagoapneto1@gmail.com>
2024-09-05 20:22:09 +0100

    Exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...otherArgs) {
+    for (let index = 0; index < otherArgs.length; index++) {
+
+        for (let i = 0; i < array.length; i++) {
+            let index2 = array.indexOf(otherArgs[index]);
+            if (index2 > -1) { // only splice array when item is found
+                array.splice(index2, 1); // 2nd parameter means remove one item only
+            }
+        }
+
+    }
+    return array
 };
 
 // Do not edit below this line
